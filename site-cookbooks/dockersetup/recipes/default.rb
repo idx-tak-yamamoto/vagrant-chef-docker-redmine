e2f856520fd2969597b9823bd2eb5361bbfb7ba9
idx-tak-yamamoto <tak-yamamoto@indexweb.co.jp>
2015-09-01 13:49:35 +0900

    not_if追加

--- site-cookbooks/dockersetup/recipes/default.rb
+++ site-cookbooks/dockersetup/recipes/default.rb
@@ -31,22 +31,26 @@ execute "pull_redmine" do
   user "root"
   command "docker pull sameersbn/redmine:latest"
   action :run
+not_if "docker images | grep 'sameersbn/redmine'"
 end
 
 execute "pull_mysql" do
   user "root"
   command "docker pull sameersbn/mysql:latest"
   action :run
+not_if "docker images | grep 'sameersbn/mysql'"
 end
 
 execute "docker_run_mysql" do
   user "root"
   command "docker run --name=mysql -d -e 'DB_NAME=redmine_production' -e 'DB_USER=redmine' -e 'DB_PASS=password' sameersbn/mysql:latest"
   action :run
+not_if "docker ps | grep 'sameersbn/mysql'"
 end
 
 execute "docker_run_redmine" do
   user "root"
   command "docker run --name=redmine -d -p 80:80 --link mysql:mysql sameersbn/redmine:latest"
   action :run
+not_if "docker ps | grep 'sameersbn/redmine'"
 end
